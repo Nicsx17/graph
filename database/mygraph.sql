577c28a1a801ddf9db25a5a15a6bf423ca57b690
Nicsx17 <130277462+Nicsx17@users.noreply.github.com>
2023-06-24 12:40:14 +0800

    Add files via upload

---
+++ database/mygraph.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 24, 2023 at 04:10 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.1.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mygraph`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `score`
+--
+
+DROP TABLE IF EXISTS `score`;
+CREATE TABLE IF NOT EXISTS `score` (
+  `playerid` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `score` int DEFAULT '0',
+  PRIMARY KEY (`playerid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `score`
+--
+
+INSERT INTO `score` (`playerid`, `score`) VALUES
+(1, 10),
+(2, 40),
+(3, 20),
+(4, 9),
+(5, 20);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
